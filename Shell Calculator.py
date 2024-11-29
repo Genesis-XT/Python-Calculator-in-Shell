329cc29c409c09d51f57677e83d1cb0fdcaa2622
Genesis-XT <genesis.xt005@gmail.com>
2024-11-29 18:10:00 +0530

    Update Shell Calculator.py

--- Shell Calculator.py
+++ Shell Calculator.py
@@ -1,11 +1,15 @@
-def add(x, y):
+#Python Shell Calculator by Genesis-XT, 29th November 2024 CE
+def add(x, y): #definition for addition
     return x+y
-def sub(x, y):
+def sub(x, y): #definition for subtraction
     return x-y
-def multi(x, y):
+def multi(x, y): #definition for multiplication
     return x*y
-def divi(x, y):
+def divi(x, y): #definition for division
     return x/y
+print ("Python Shell Calculator by Genesis-XT")
+print (" ")
+#selection operation
 print ("Select Operation:-")
 print ("1. Addition")
 print ("2. Subtraction")
@@ -13,7 +17,7 @@ print ("3. Multiplication")
 print ("4. Division")
 while True:
     choice = input("Enter Choice:- [1/2/3/4]:- ")
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4'): #checking if correct choice is entered
         try:
             num1 = float(input("Enter first number:- "))
             num2 = float(input("Enter second number:- "))
@@ -31,7 +35,7 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divi(num1, num2))
-    next_calculation = input("Let's do next calculation? [Yes/No]:- ")
+    next_calculation = input("Let's do next calculation? [Yes/No]:- ") #multiple calculations
     if next_calculation == "no":
           break
     else:
